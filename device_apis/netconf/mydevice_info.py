ba0f1916002b2f30564fc77ecda07e0e844dff0c
zdh45222 <zdh45222@163.com>
2021-07-22 21:30:24 +0800

    Create mydevice_info

---
+++ device_apis/netconf/mydevice_info.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python
+"""Device Details for spare switch
+We are testing NETCONF for Cisco Catalyst 9k switch IOS 16.12.05b 
+
+"""
+
+__author__ = "Jacky Zhang"
+__author_email__ = "zdh45222@163.com"
+
+ios_xe1 = {
+             "address": "10.21.1.254",
+             "netconf_port": 830,
+             "restconf_port": 9443,
+             "ssh_port": 22,
+             "username": "lznwfy",
+             "password": "<ki8(ol./;p0-['a"
+          }
+
