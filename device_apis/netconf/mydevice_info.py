1b398d1aaab2872b2ed8162747bf4f02e01bf8a9
zdh45222 <zdh45222@163.com>
2021-07-23 16:12:58 +0800

    Update mydevice_info,py add MakatiSW

--- device_apis/netconf/mydevice_info.py
+++ device_apis/netconf/mydevice_info.py
@@ -7,12 +7,14 @@ We are testing NETCONF for Cisco Catalyst 9k switch IOS 16.12.05b
 __author__ = "Jacky Zhang"
 __author_email__ = "zdh45222@163.com"
 
-ios_xe1 = {
-             "address": "10.21.1.254",
+MakatiSW = {
+             "address": "172.21.26.139",
              "netconf_port": 830,
              "restconf_port": 9443,
              "ssh_port": 22,
              "username": "lznwfy",
              "password": "<ki8(ol./;p0-['a"
           }
+
+
 
